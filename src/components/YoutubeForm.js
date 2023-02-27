143c994cf0fef3280807256ffe1d4e7e4b5c473a
Amar Singh Yadav <amarprince9@gmail.com>
2023-02-27 21:48:17 +0530

    Intial setup of Formik Library

---
+++ src/components/YoutubeForm.js
@@ -0,0 +1,50 @@
+import { useFormik } from 'formik';
+import React from 'react';
+
+function YoutubeForm() {
+  const formik = useFormik({
+    initialValues: {
+      name: 'Amar',
+      email: '',
+      channel: '',
+    },
+  });
+
+  console.log('Form Values', formik.values);
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        />
+
+        <label>E-mail</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+
+        <label>Channel</label>
+        <input
+          type="text"
+          id="channel"
+          name="channel"
+          onChange={formik.handleChange}
+          value={formik.values.channel}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default YoutubeForm;
