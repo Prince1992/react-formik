80bb968d51ab242afa8060a8f1555e52d9ae0f84
Amar Singh Yadav <amarprince9@gmail.com>
2023-02-28 22:31:55 +0530

    Form Validation

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,16 +1,38 @@
 import { useFormik } from 'formik';
 import React from 'react';
 
+const initialValues = {
+  name: '',
+  email: '',
+  channel: '',
+};
+
+const onSubmit = (values) => {
+  console.log('Submit', values);
+};
+
+const validate = (values) => {
+  let errors = {};
+  if (!values.name) {
+    errors.name = 'Required';
+  }
+  if (!values.email) {
+    errors.email = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email';
+  }
+  if (!values.channel) {
+    errors.channel = 'Required';
+  }
+
+  return errors;
+};
+
 function YoutubeForm() {
   const formik = useFormik({
-    initialValues: {
-      name: 'Amar',
-      email: '',
-      channel: '',
-    },
-    onSubmit: (values) => {
-      console.log('Form Data', values);
-    },
+    initialValues,
+    onSubmit,
+    validate,
   });
 
   //console.log('Form Values', formik.values);
